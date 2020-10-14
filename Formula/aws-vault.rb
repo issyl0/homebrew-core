9d05cd91f4a50920e57fb470fda318f026d66c6b
Issy Long <me@issyl0.co.uk>
2020-10-14 10:02:23 +0100

    aws-vault (new formula)
    - This is a Linux-only formula. The macOS version ships as a Cask.

---
+++ Formula/aws-vault.rb
@@ -0,0 +1,25 @@
+class AwsVault < Formula
+  desc "Securely store and access AWS credentials in development environments"
+  homepage "https://github.com/99designs/aws-vault"
+  url "https://github.com/99designs/aws-vault/archive/v6.2.0.tar.gz"
+  sha256 "39886f4bc3985d4aefbae6fc88532499ac2c39cbabc33d860bba6d355158e17d"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on :linux
+
+  def install
+    flags = "-X main.Version=#{version} -s -w"
+
+    system "go", "build", "-ldflags=#{flags}"
+    bin.install "aws-vault"
+
+    zsh_completion.install "contrib/completions/zsh/aws-vault.zsh"
+    bash_completion.install "contrib/completions/bash/aws-vault.bash"
+  end
+
+  test do
+    assert_match("aws-vault: error: required argument 'profile' not provided, try --help",
+      shell_output("#{bin}/aws-vault login 2>&1", 1))
+  end
+end
